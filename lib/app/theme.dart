912cb1b365dd54dc891b04dfd6ebd341c8cd73fd
Fran Marić <fran.maric.vk@gmail.com>
2021-10-07 20:31:59 +0200

    Add custom theme

--- lib/app/theme.dart
+++ lib/app/theme.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/app/colors.dart';
+import 'package:movie_app/app/text_theme.dart';
 
 final theme = ThemeData(
   primarySwatch: MaterialColor(0xFFFEE715, primaryMaterialColor),
   scaffoldBackgroundColor: blackBackground,
+  textTheme: textTheme,
 );
