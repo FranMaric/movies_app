71f02c7b781c8b2e2daecf2390b405fb37c2e82d
Fran Marić <fran.maric.vk@gmail.com>
2021-10-12 15:20:01 +0200

    Adjust theme for text field theming

--- lib/app/theme.dart
+++ lib/app/theme.dart
@@ -2,8 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:movie_app/app/colors.dart';
 import 'package:movie_app/app/text_theme.dart';
 
+final outlineInputBorder = OutlineInputBorder(
+  borderRadius: BorderRadius.circular(100.0),
+  borderSide: const BorderSide(
+    color: Colors.white,
+  ),
+);
+
 final theme = ThemeData(
   primarySwatch: MaterialColor(0xFFFEE715, primaryMaterialColor),
   scaffoldBackgroundColor: blackBackground,
   textTheme: textTheme,
+  inputDecorationTheme: InputDecorationTheme(
+    hintStyle: const TextStyle(
+      color: Colors.white,
+    ),
+    contentPadding: const EdgeInsets.all(15.0),
+    border: outlineInputBorder,
+    focusedBorder: outlineInputBorder,
+    enabledBorder: outlineInputBorder,
+  ),
+  iconTheme: const IconThemeData(
+    color: Colors.white,
+  ),
 );
