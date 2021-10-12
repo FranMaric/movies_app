d9ff1602b121c915fceb2e49918a803d8ff7cd74
Fran Marić <fran.maric.vk@gmail.com>
2021-10-12 23:32:51 +0200

    In progress

--- lib/domain/repositories/movies_repository/movies_repository_impl.dart
+++ lib/domain/repositories/movies_repository/movies_repository_impl.dart
@@ -1,6 +1,7 @@
 import 'package:movie_app/domain/models/movie.dart';
 import 'package:movie_app/domain/repositories/movies_repository/movies_repository.dart';
 import 'package:movie_app/source_remote/api_repository/api_repository.dart';
+import 'package:movie_app/extensions/nullable_int_extension.dart';
 
 class MoviesRepositoryImpl implements MoviesRepository {
   MoviesRepositoryImpl({required ApiRepository apiRepository}) : _apiRepository = apiRepository;
@@ -10,5 +11,23 @@ class MoviesRepositoryImpl implements MoviesRepository {
   @override
   Future<List<Movie>> searchMovies({required int page, required String query}) {
     throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Movie>> getTopRatedMovies({required int page}) async {
+    // try {
+    final response = await _apiRepository.getTopRatedMovies(page: page);
+
+    if (response.statusCode.isSuccessful) {
+      final movies = List<Map<String, dynamic>>.from(response.data['results'] as List).map((movie) => Movie.fromJson(movie)).toList();
+
+      return movies;
+    }
+
+    return [];
+    // } catch (e) {
+    //   print(e);
+    //   return [];
+    // }
   }
 }
