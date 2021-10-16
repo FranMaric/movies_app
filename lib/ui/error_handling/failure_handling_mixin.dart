febd7f0b39b61382b965ace05c07e0cc39fe3a4f
Fran Marić <fran.maric.vk@gmail.com>
2021-10-16 17:34:52 +0200

    Move Failure to String conversion inside Failure and adjusted text font

--- lib/ui/error_handling/failure_handling_mixin.dart
+++ lib/ui/error_handling/failure_handling_mixin.dart
@@ -1,27 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/domain/failure.dart';
 
-/// Inspired by https://github.com/DMesek/cool_companies/blob/master/lib/common/presentation/failure_handling_mixin.dart
 mixin FailureHandlingMixin on Widget {
-  void displayAppropriateMessage({
+  void showSnackBarMessage({
     required BuildContext context,
     required Failure failure,
   }) {
-    failure.when(
-      generic: (message) => displaySnackbar(context, message),
-      noNetwork: () => displaySnackbar(context, 'Op, nema interneta!'),
-      unauthorized: () => displaySnackbar(context, 'Op, nemas pristup!'),
-      serverError: () => displaySnackbar(context, 'Backend je opet zeznuo!'),
-    );
-  }
-
-  void displaySnackbar(BuildContext context, String message) {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         duration: const Duration(seconds: 2),
         content: Text(
-          message,
-          style: Theme.of(context).textTheme.headline6?.copyWith(color: Colors.white),
+          failure.toString(),
+          style: Theme.of(context).textTheme.subtitle2?.copyWith(color: Colors.white),
         ),
       ),
     );
