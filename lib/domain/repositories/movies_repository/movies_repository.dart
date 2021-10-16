06f79f6a6a8b6eed49b982438789cf7ce161452f
Fran Marić <fran.maric.vk@gmail.com>
2021-10-16 15:18:32 +0200

    Add Either as return type

--- lib/domain/repositories/movies_repository/movies_repository.dart
+++ lib/domain/repositories/movies_repository/movies_repository.dart
@@ -1,6 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:movie_app/domain/failure.dart';
 import 'package:movie_app/domain/models/movie.dart';
 
 abstract class MoviesRepository {
-  Future<List<Movie>> searchMovies({required int page, required String query});
-  Future<List<Movie>> getTopRatedMovies({required int page});
+  Future<Either<Failure, List<Movie>>> searchMovies({required int page, required String query});
+  Future<Either<Failure, List<Movie>>> getTopRatedMovies({required int page});
 }
--- lib/domain/repositories/movies_repository/movies_repository_impl.dart
+++ lib/domain/repositories/movies_repository/movies_repository_impl.dart
@@ -1,7 +1,10 @@
+import 'package:movie_app/domain/failure.dart';
 import 'package:movie_app/domain/models/movie.dart';
 import 'package:movie_app/domain/repositories/movies_repository/movies_repository.dart';
 import 'package:movie_app/source_remote/api_repository/api_repository.dart';
 import 'package:movie_app/extensions/nullable_int_extension.dart';
+
+import 'package:dartz/dartz.dart';
 
 class MoviesRepositoryImpl implements MoviesRepository {
   MoviesRepositoryImpl({required ApiRepository apiRepository}) : _apiRepository = apiRepository;
@@ -9,25 +12,36 @@ class MoviesRepositoryImpl implements MoviesRepository {
   final ApiRepository _apiRepository;
 
   @override
-  Future<List<Movie>> searchMovies({required int page, required String query}) {
-    throw UnimplementedError();
+  Future<Either<Failure, List<Movie>>> searchMovies({required int page, required String query}) async {
+    try {
+      final response = await _apiRepository.searchMovies(page: page, query: query);
+
+      if (response.statusCode.isSuccessful) {
+        final movies = List<Map<String, dynamic>>.from(response.data['results'] as List).map((movie) => Movie.fromJson(movie)).toList();
+
+        return Right(movies);
+      }
+
+      return Left(Failure.fromResponse(response));
+    } catch (e) {
+      return Left(Failure.generic(e.toString()));
+    }
   }
 
   @override
-  Future<List<Movie>> getTopRatedMovies({required int page}) async {
-    // try {
-    final response = await _apiRepository.getTopRatedMovies(page: page);
+  Future<Either<Failure, List<Movie>>> getTopRatedMovies({required int page}) async {
+    try {
+      final response = await _apiRepository.getTopRatedMovies(page: page);
 
-    if (response.statusCode.isSuccessful) {
-      final movies = List<Map<String, dynamic>>.from(response.data['results'] as List).map((movie) => Movie.fromJson(movie)).toList();
+      if (response.statusCode.isSuccessful) {
+        final movies = List<Map<String, dynamic>>.from(response.data['results'] as List).map((movie) => Movie.fromJson(movie)).toList();
 
-      return movies;
+        return Right(movies);
+      }
+
+      return Left(Failure.fromResponse(response));
+    } catch (e) {
+      return Left(Failure.generic(e.toString()));
     }
-
-    return [];
-    // } catch (e) {
-    //   print(e);
-    //   return [];
-    // }
   }
 }
