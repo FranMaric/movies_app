f348e9601915dd80c17925502711ecf470c02470
Fran Marić <fran.maric.vk@gmail.com>
2021-10-16 15:17:58 +0200

    Create failure_handling_mixin.dart

---
+++ lib/ui/error_handling/failure_handling_mixin.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/domain/failure.dart';
+
+/// Inspired by https://github.com/DMesek/cool_companies/blob/master/lib/common/presentation/failure_handling_mixin.dart
+mixin FailureHandlingMixin on Widget {
+  void displayAppropriateMessage({
+    required BuildContext context,
+    required Failure failure,
+  }) {
+    failure.when(
+      generic: (message) => displaySnackbar(context, message),
+      noNetwork: () => displaySnackbar(context, 'Op, nema interneta!'),
+      unauthorized: () => displaySnackbar(context, 'Op, nemas pristup!'),
+      serverError: () => displaySnackbar(context, 'Backend je opet zeznuo!'),
+    );
+  }
+
+  void displaySnackbar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 1),
+        content: Text(
+          message,
+          style: Theme.of(context).textTheme.headline2?.copyWith(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
