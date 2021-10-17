b5496b6dd5a9af0fb332c3a73edf57f57db0c135
Fran Marić <fran.maric.vk@gmail.com>
2021-10-17 23:31:38 +0200

    Create movie_details_screen.dart

---
+++ lib/ui/movie_details/movie_details_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MovieDetailsScreen extends StatelessWidget {
+  const MovieDetailsScreen({Key? key}) : super(key: key);
+
+  static const route = '/movie_details';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
