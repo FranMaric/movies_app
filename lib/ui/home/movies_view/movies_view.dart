f40c65220a0a41eae9929485e7fa1e1a8bfdf43d
Fran Marić <fran.maric.vk@gmail.com>
2021-10-18 15:41:11 +0200

    Moved scrollController outside of class and add ScrollToTopFAB

--- lib/ui/home/movies_view/movies_view.dart
+++ lib/ui/home/movies_view/movies_view.dart
@@ -5,17 +5,19 @@ import 'package:movie_app/ui/error_handling/failure_handling_mixin.dart';
 import 'package:movie_app/ui/home/movies_view/movie_widget.dart';
 import 'package:movie_app/ui/home/movies_view/provider/movies_notifier_provider.dart';
 import 'package:movie_app/ui/home/movies_view/provider/movies_state.dart';
+import 'package:movie_app/ui/home/widgets/scroll_to_top_fab.dart';
 import 'package:movie_app/ui/home/widgets/search_bar.dart';
 
-class MoviesView extends StatelessWidget with FailureHandlingMixin {
-  MoviesView({Key? key}) : super(key: key);
+// _scrollController is not inside MoviesView class to keep scroll offset when switching MoviesView and ActorsView
+final _scrollController = ScrollController(keepScrollOffset: true);
 
-  final scrollController = ScrollController(keepScrollOffset: true);
+class MoviesView extends StatelessWidget with FailureHandlingMixin {
+  const MoviesView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     void scrollControllerListener() {
-      if (scrollController.position.pixels + 50 > scrollController.position.maxScrollExtent) {
+      if (_scrollController.position.pixels + 50 > _scrollController.position.maxScrollExtent) {
         context.read(moviesViewNotifierProvider.notifier).onNextPage();
       }
     }
@@ -31,59 +33,69 @@ class MoviesView extends StatelessWidget with FailureHandlingMixin {
         }
       },
       child: StatefulWrapper(
-        initState: () => scrollController.addListener(scrollControllerListener),
-        dispose: () => scrollController.removeListener(scrollControllerListener),
-        child: Column(
+        initState: () => _scrollController.addListener(scrollControllerListener),
+        dispose: () => _scrollController.removeListener(scrollControllerListener),
+        child: Stack(
+          fit: StackFit.expand,
           children: [
-            SearchBar(
-              hintText: 'Search movies',
-              onSearch: context.read(moviesViewNotifierProvider.notifier).onSearch,
-            ),
-            Expanded(
-              child: Consumer(
-                builder: (context, watch, child) {
-                  return watch(moviesViewNotifierProvider).when(
-                    initial: () => Container(),
-                    loading: () => const Center(child: CircularProgressIndicator()),
-                    failure: (failure) => Center(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          failure.toString(),
-                          style: Theme.of(context).textTheme.subtitle1,
-                          textAlign: TextAlign.center,
+            Column(
+              children: [
+                SearchBar(
+                  hintText: 'Search movies',
+                  onSearch: context.read(moviesViewNotifierProvider.notifier).onSearch,
+                ),
+                Expanded(
+                  child: Consumer(
+                    builder: (context, watch, child) {
+                      return watch(moviesViewNotifierProvider).when(
+                        initial: () => Container(),
+                        loading: () => const Center(child: CircularProgressIndicator()),
+                        failure: (failure) => Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              failure.toString(),
+                              style: Theme.of(context).textTheme.subtitle1,
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                    data: (page, isLoadingNextPage, query, movies) {
-                      return RefreshIndicator(
-                        onRefresh: context.read(moviesViewNotifierProvider.notifier).onRefresh,
-                        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-                        child: ListView.builder(
-                            controller: scrollController,
-                            itemCount: movies.length + (isLoadingNextPage ? 1 : 0),
-                            itemBuilder: (context, index) {
-                              if (index < movies.length) {
-                                return MovieWidget(
-                                  movie: movies[index],
-                                  onTap: () {
-                                    //TODO: Navigate to details
-                                  },
-                                );
-                              }
+                        data: (page, isLoadingNextPage, query, movies) {
+                          return RefreshIndicator(
+                            onRefresh: context.read(moviesViewNotifierProvider.notifier).onRefresh,
+                            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                            child: ListView.builder(
+                                controller: _scrollController,
+                                itemCount: movies.length + (isLoadingNextPage ? 1 : 0),
+                                itemBuilder: (context, index) {
+                                  if (index < movies.length) {
+                                    return MovieWidget(
+                                      movie: movies[index],
+                                      onTap: () {
+                                        //TODO: Navigate to details
+                                      },
+                                    );
+                                  }
 
-                              return Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: CircularProgressIndicator(
-                                  color: Theme.of(context).primaryColor,
-                                ),
-                              );
-                            }),
+                                  return Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: CircularProgressIndicator(
+                                      color: Theme.of(context).primaryColor,
+                                    ),
+                                  );
+                                }),
+                          );
+                        },
                       );
                     },
-                  );
-                },
-              ),
+                  ),
+                ),
+              ],
+            ),
+            Positioned(
+              right: 15,
+              bottom: 15,
+              child: ScrollToTopFAB(scrollController: _scrollController),
             ),
           ],
         ),
