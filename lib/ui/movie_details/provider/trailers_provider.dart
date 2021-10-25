40b3f8542292e1b0236be351fffaf56d8ef41176
Fran Marić <fran.maric.vk@gmail.com>
2021-10-25 14:50:44 +0200

    Better parsing

--- lib/ui/movie_details/provider/trailers_provider.dart
+++ lib/ui/movie_details/provider/trailers_provider.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movie_app/domain/models/failure.dart';
 import 'package:movie_app/domain/models/trailer.dart';
 import 'package:movie_app/domain/repositories/movies_repository/movies_repository_provider.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 final trailersProvider = FutureProvider.family<String, int>((ref, movieId) async {
   final moviesRepository = ref.watch(moviesRepositoryProvider);
@@ -10,7 +11,7 @@ final trailersProvider = FutureProvider.family<String, int>((ref, movieId) async
 
   return trailersOrFailure.fold(
     (failure) => throw failure,
-    (trailers) => _selectTrailer(trailers, movieId),
+    (trailers) => _correctVideoId(_selectTrailer(trailers, movieId), movieId),
   );
 });
 
@@ -18,7 +19,25 @@ final trailersProvider = FutureProvider.family<String, int>((ref, movieId) async
 String _selectTrailer(List<Trailer> trailers, int movieId) {
   for (final trailer in trailers) {
     if (trailer.site == 'YouTube') {
-      return 'https://www.youtube.com/watch?v=${trailer.key}';
+      return trailer.key;
+    }
+  }
+
+  throw Failure.noTrailer(movieId);
+}
+
+String _correctVideoId(String videoId, int movieId) {
+  if (!videoId.contains('http') && (videoId.length == 11)) return videoId;
+  videoId = videoId.trim();
+
+  for (var exp in [
+    RegExp(r'^https:\/\/(?:www\.|m\.)?youtube\.com\/watch\?v=([_\-a-zA-Z0-9]{11}).*$'),
+    RegExp(r'^https:\/\/(?:www\.|m\.)?youtube(?:-nocookie)?\.com\/embed\/([_\-a-zA-Z0-9]{11}).*$'),
+    RegExp(r'^https:\/\/youtu\.be\/([_\-a-zA-Z0-9]{11}).*$')
+  ]) {
+    Match? match = exp.firstMatch(videoId);
+    if (match != null && match.groupCount >= 1 && match.group(1) != null) {
+      return match.group(1)!;
     }
   }
 
